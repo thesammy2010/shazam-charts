06c09bb2603ede20b5c6d1b3421a30befa6aea94
thesammy2010 <43960199+thesammy2010@users.noreply.github.com>
2021-08-15 14:57:57 +0100

    Refactored cli.py and added test
    This commit refactors the CLI method to allow for correct testing and adds unit and end-to-end tests for the CLI

--- shazam_charts/run.py
+++ shazam_charts/run.py
@@ -2,6 +2,9 @@ import argparse
 import logging
 import re
 import sys
+import typing
+
+from shazam_charts.transform import transform
 
 
 # define cli argument parser
@@ -14,10 +17,44 @@ parser: argparse.ArgumentParser = argparse.ArgumentParser(
 )
 
 # add CLI arguments
-parser.add_argument("type", type=str, help="Chart type")
+parser.add_argument("method", type=str, help="Chart type")
 parser.add_argument("count", type=int, help="The number of records returned ordered by highest to lowest")
 parser.add_argument("-v", "--verbose", action="store_true", help="Set output verbosity")
 parser.add_argument("-h", "--help", action="help", help="Display this message")
+
+
+def parse_args(args: typing.List[str]) -> (argparse.Namespace, typing.List[str]):
+    """
+    Method to parse string arguments
+    :param args: List of arguments as a string
+    :return: A tuple of (known arguments in a namespace, list of unparsed args)
+    """
+    return parser.parse_known_args(args)
+
+
+def are_args_valid(args: argparse.Namespace, unknown: typing.List[str] = None) -> bool:
+    """
+    Determines if arguments are valid and returns a bool stating whether they are valid
+    :param args: Namespace object of parsed arguments
+    :param unknown: List of unparsed string arguments
+    :return: True if the arguments are valid and false otherwise
+    """
+    # notify if extra CLI arguments are passed
+    if unknown:
+        logging.warning(f"Unknown arguments {unknown} passed and are ignored")
+        return False
+
+    # check if count is valid
+    if args.count <= 0:
+        logging.error(f"Count {args.count} is invalid. Must be greater than 0")
+        return False
+
+    # check if chart type is valid
+    if not re.search(r"(chart|state_chart)$", args.method):
+        logging.error(f"Chart type {args.method} is invalid. Must be one of [chart, state_chart]")
+        return False
+
+    return True
 
 
 def main() -> None:
@@ -27,7 +64,7 @@ def main() -> None:
     """
 
     # parse arguments
-    args, unknown = parser.parse_known_args()
+    args, unknown = parse_args(sys.argv[1:])
 
     # handle verbosity
     if args.verbose:
@@ -35,21 +72,14 @@ def main() -> None:
     else:
         logging.basicConfig(level=logging.INFO, format="%(message)s")
 
-    # notify if extra CLI arguments are passed
-    if unknown:
-        logging.warning(f"Unknown arguments {unknown} passed and are ignored")
-
-    # check if count is valid
-    if args.count <= 0:
-        logging.error(f"Count {args.count} is invalid. Must be greater than 0")
+    # validate args
+    if not are_args_valid(args=args, unknown=unknown):
         sys.exit(1)
 
-    # check if chart type is valid
-    if not re.search(r"(chart|state_chart)$", args.type):
-        logging.error(f"Chart type {args.type} is invalid. Must be one of [chart, state_chart]")
-        sys.exit(1)
+    # run main method
+    transform(method=args.method, count=args.count)
 
-    # exit as no other code has been implemented
+    # exit gracefully
     sys.exit(0)
 
 
